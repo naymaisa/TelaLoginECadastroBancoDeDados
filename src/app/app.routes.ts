37780eaddcb56f362419182f3301a5d3d0a69638
Nayara <nayaramaisa.stn@gmail.com>
2025-06-22 11:10:34 -0300

    finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent:()=>
+      import('./pages/auth/auth.component').then(m=> m.AuthComponent)
+  },
+    {
+    path: 'home',
+    loadComponent:()=>
+      import('./pages/home/home.component').then(m=> m.HomeComponent)
+  }
+];
